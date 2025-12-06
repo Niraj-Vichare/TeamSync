b2a3492436ff4e7a3f5d09bb1a7e664b7be57d86
Niraj Vichare <nirajkvichare123@gmail.com>
2025-12-06 16:44:29 +0530

    Update the hub for realtime updates inside the leaderboard.

---
+++ collab-backend/Enterprise.Flowstate.Core/Hubs/LeaderboardHub.cs
@@ -0,0 +1,36 @@
+﻿using Enterprise.Flowstate.BAL.Interface.Service;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Enterprise.Flowstate.Hubs
+{
+    public class LeaderboardHub:Hub
+    {
+        private IOmniService _omniService;
+        public LeaderboardHub(IOmniService omniService)
+        {
+            _omniService = omniService;
+        }
+
+        public async Task JoinWorkspace(string workspaceId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"workspace_{workspaceId}");
+            await Clients.Caller.SendAsync("Joined", $"Joined workspace {workspaceId}");
+        }
+
+        public async Task LeaveWorkspace(string workspaceId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, $"workspace_{workspaceId}");
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("Connected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
